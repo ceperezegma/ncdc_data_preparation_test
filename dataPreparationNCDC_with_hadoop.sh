c0c6fb9f1b53ac495ece2151e1de63a316b3467d
ceperezegma <perez.carloseduardo@gmail.com>
2020-01-05 15:40:50 +0100

    change of directoryBase matching EMR fs structure

--- dataPreparationNCDC_with_hadoop.sh
+++ dataPreparationNCDC_with_hadoop.sh
@@ -4,7 +4,7 @@
 read offset s3file
 
 # local directory used to copy the data files from s3
-directoryBase=/hadoop/user/carlos/ncdc-dataset-sample/
+directoryBase=/user/carlos/ncdc-dataset-sample/
 
 # retrieve file from s3 to local disk
 echo "reporter:stats:retrieving $s3file" >&2
