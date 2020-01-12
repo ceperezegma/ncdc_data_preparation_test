e5b1ffe177bcd5e4f536fc9f81987f01da561519
ceperezegma <perez.carloseduardo@gmail.com>
2020-01-12 22:38:48 +0100

    change local directory base

--- dataPreparationNCDC_with_hadoop_2.0.sh
+++ dataPreparationNCDC_with_hadoop_2.0.sh
@@ -4,7 +4,7 @@
 read offset s3file
 
 # local directory used to copy the data files from s3
-directoryBase=/user/carlos/ncdc-dataset-sample/
+directoryBase=/home/hadoop/ncdc_data_preparation_test/ncdc_sample/
 
 # retrieve file from s3 to local disk
 echo "reporter:status:retrieving $s3file" >&2
