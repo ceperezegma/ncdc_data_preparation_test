616c124faee609e7757031760d69263d3554773f
ceperezegma <perez.carloseduardo@gmail.com>
2020-01-12 22:59:02 +0100

    remove the directoryBase variable

--- dataPreparationNCDC_with_hadoop_2.0.sh
+++ dataPreparationNCDC_with_hadoop_2.0.sh
@@ -3,9 +3,6 @@
 # NLineInputFormat gives a single line: key is offset, value is the s3 URI
 read offset s3file
 
-# local directory used to copy the data files from s3
-directoryBase=/home/hadoop/ncdc_data_preparation_test/ncdc_sample/
-
 # retrieve file from s3 to local disk
 echo "reporter:status:retrieving $s3file" >&2
-hadoop fs -get $s3file $directoryBase
+hadoop fs -get $s3file .
