656924d98d01a120de7c535a0cbded73f8f3806c
ceperezegma <perez.carloseduardo@gmail.com>
2020-01-22 20:11:29 +0100

    cahnge bash script and add tab to list of s3 files

---
+++ dataPrep.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# NLineInputFormat gives a single line: key is offset, value is the s3 URI
+read offset s3file
+# retrieve file from s3 to local disk
+#echo "reporter:status:Retrieving $s3file" >&2
+hadoop fs -get $s3file .
