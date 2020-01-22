c4c4a4068485a9b8c31f30daee91dac6fe867a3d
ceperezegma <perez.carloseduardo@gmail.com>
2020-01-22 20:36:10 +0100

    small changes bash file

--- dataPrep.sh
+++ dataPrep.sh
@@ -2,5 +2,5 @@
 # NLineInputFormat gives a single line: key is offset, value is the s3 URI
 read offset s3file
 # retrieve file from s3 to local disk
-#echo "reporter:status:Retrieving $s3file" >&2
-hadoop fs -get $s3file .
+echo "reporter:status:retrieving $s3file" >&2
+hadoop fs -get $s3file ~/ncdc/
